722641340320f7119fecc53f9e79a4b099dec3cd
卢方舟 <735417031@qq.com>
2022-10-27 09:40:25 +0800

    feat: init project, alias

---
+++ src/components/TestCom.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="test-com">
+    23123
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@a': path.resolve(__dirname, 'src/assets'),
+      '@c': path.resolve(__dirname, 'src/components')
+    }
+  },
+})

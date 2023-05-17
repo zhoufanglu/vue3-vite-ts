5f7b9f75741711c54acea3c43a77a8fc97a3badd
lufangzhou <lufangzhou@gmail.com>
2023-05-17 15:00:17 +0800

    refactor: vite config .js

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,43 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import type { ConfigEnv } from 'vite'
 import * as path from 'path'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    DefineOptions(),
-    vueSetupExtend(),
-    AutoImport({ imports: ['vue', 'vue-router', 'pinia'] }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@a': path.resolve(__dirname, 'src/assets'),
-      '@c': path.resolve(__dirname, 'src/components'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/styles/entrance.scss";',
+export default ({ mode, command }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd()) // 获取.env文件里定义的环境变量
+  return defineConfig({
+    base: './',
+    plugins: [
+      vue(),
+      DefineOptions(),
+      vueSetupExtend(),
+      AutoImport({ imports: ['vue', 'vue-router', 'pinia'] }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@a': path.resolve(__dirname, 'src/assets'),
+        '@c': path.resolve(__dirname, 'src/components'),
       },
     },
-  },
-})
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/entrance.scss";',
+        },
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+    },
+    build: {
+      sourcemap: false,
+      outDir: env.VITE_OUTPUT_DIR,
+      assetsDir: 'assets',
+    },
+  })
+}

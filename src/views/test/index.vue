ea156140ce751c13f2acc813d421e1345e0e7963
lufangzhou <lufangzhou@gmail.com>
2024-07-08 15:29:06 +0800

    feat:  tree test

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -8,6 +8,8 @@
     <div class="bg">我是背景</div>
     {{ searchForm }}
     <childCom v-model:searchForm="searchForm"></childCom>
+    <h3>树的家长</h3>
+    <el-tree style="max-width: 600px" :data="data" :props="defaultProps" />
   </div>
 </template>
 <script lang="ts">
@@ -35,6 +37,40 @@
   const searchForm = ref({})
   const switchTheme = (val: string) => {
     document.querySelector('body')!.setAttribute('data-theme', val)
+  }
+  /** ********************tree***********************/
+  const data: any[] = [
+    {
+      label: 'Level one 1',
+      isLeaf: false,
+      children: [],
+    },
+    {
+      label: 'Level one 2',
+      children: [
+        {
+          label: 'Level two 2-1',
+          children: [
+            {
+              label: 'Level three 2-1-1',
+            },
+          ],
+        },
+        {
+          label: 'Level two 2-2',
+          children: [
+            {
+              label: 'Level three 2-2-1',
+            },
+          ],
+        },
+      ],
+    },
+  ]
+  const defaultProps = {
+    children: 'children',
+    label: 'label',
+    isLeaf: 'isLeaf',
   }
 </script>
 
